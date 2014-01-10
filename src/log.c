3c4483a65e856325b0cbb15acbcbe0b4d90fd933
Vadim Kochan <vadim4j@gmail.com>
2014-01-10 22:25:30 +0200

    Added syslog logging mechanism

---
+++ src/log.c
@@ -0,0 +1,35 @@
+/*
+ * Copyright (C) 2013 Vadim Kochan <vadim4j@gmail.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+ 
+#include <stdio.h>
+#include <stdarg.h>
+#include <syslog.h>
+
+#include "log.h"
+
+int
+#ifdef __GNUC__
+__attribute__((format(printf, 2, 3)))
+#endif
+nlevtd_log(int level, const char *fmt, ...)
+{
+    va_list args;
+
+    va_start(args, fmt);
+    vsyslog(level, fmt, args);
+    va_end(args);
+}
