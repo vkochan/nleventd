23547bc48887aa092dbc8256041e438ff433f89d
Vadim Kochan <vadim4j@gmail.com>
2014-01-11 03:01:57 +0200

    Updated samples: HOWTO & if_addr.sh

--- samples/scripts/if_addr.sh
+++ samples/scripts/if_addr.sh
@@ -1,3 +1,13 @@
 #! /bin/sh
 
-echo "From $0: Event is [$NL_EVENT]"
+if [ "$NL_EVENT" = "NEWADDR" ]
+then
+    ADDR_OP="ADD"
+    DIRECT="to"
+else
+    ADDR_OP="DEL"
+    DIRECT="from"
+fi
+
+echo "Address $ADDR_OP: <$NL_ADDRESS/$NL_PREFIXLEN, $NL_SCOPE> $DIRECT $NL_IFNAME interface"
+echo ""
